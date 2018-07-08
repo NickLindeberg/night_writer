c871cd78350e3a170b15925bde5c894fd7d05ebf
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-07-08 09:43:33 -0600

    adds test for alphabet class

--- test/alphabet_to_braille_test.rb
+++ test/alphabet_to_braille_test.rb
@@ -3,4 +3,11 @@ require 'minitest/pride'
 require 'pry'
 require './lib/alphabet_to_braille.rb'
 
-class AlphabetToBrailleTest <Minitest::
+class AlphabetToBrailleTest < Minitest::Test
+
+  def test_it_exists
+    atb = AlphabetToBraille.new
+    assert_instance_of AlphabetToBraille, atb
+  end
+
+end
