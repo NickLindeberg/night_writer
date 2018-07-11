d320e04c1343c99a4c8c6b3d3d7ba683437d5292
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-07-11 10:46:40 -0600

    update

---
+++ lib/night_reader.rb
@@ -0,0 +1,26 @@
+require './lib/braille_dictionary.rb'
+require 'pry'
+
+class NightReader
+  attr_reader :print_confirm
+
+  def initialize
+    @braille_dictionary = BrailleDictionary.new.alphabet_keys
+    @braille_reader = File.open(ARGV[0], "r")
+    @incoming_braille_text = @braille_reader.read
+    @braille_reader.close
+  end
+
+  def print_confirm #NOT COMPLETE YET
+    number_of_characters = @incoming_braille_text.length - 2
+    puts "Created '#{ARGV[1]}' containing #{number_of_characters} characters"
+  end
+
+  def translate_to_english(braille_message)
+
+
+  end
+end
+
+nr = NightReader.new
+nr.print_confirm
