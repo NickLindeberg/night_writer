5ce1b1d66e943d549a4ecb656d84d4b35bd3c35f
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-07-11 17:33:44 -0600

    adds split test for night_reader

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -6,13 +6,19 @@ class NightReader
                 :incoming_braille_text,
                 :all_one_line,
                 :b_to_e_dictionary,
-                :braille_reader
+                :braille_reader,
+                :line_1,
+                :line_2,
+                :line_3
 
   def initialize
     @b_to_e_dictionary = BrailleDictionary.new.braille_keys
     @braille_reader = File.open(ARGV[0], "r")
     @incoming_braille_text = @braille_reader.read
     @braille_reader.close
+    @line_1 = []
+    @line_2 = []
+    @line_3 = []
   end
 
   def print_confirm #NOT COMPLETE YET
@@ -20,12 +26,17 @@ class NightReader
     puts "Created '#{ARGV[1]}' containing #{number_of_characters} unicorns"
   end
 
-  def format_incoming_text(incoming_braille_text)
-    incoming_braille_text.delete("\n")
+  # def format_incoming_text(incoming_braille_text)
+  #   incoming_braille_text.delete("\n")
+  # end
+  def split(incoming_text)
+    @line_1, @line_2, @line_3 = incoming_text.split(/\n/)
   end
 
-  def scan_to_array(all_one_line)
-    all_one_line.scan(/../)
+  # def scan_to_array(all_one_line)
+  #   all_one_line[0..39].scan(/../)
+  # end
+  def line_to_array
   end
 
   def read_incoming_braille(braille_input)
--- test/night_reader_test.rb
+++ test/night_reader_test.rb
@@ -1,7 +1,7 @@
 require "minitest/autorun"
 require 'minitest/pride'
 require 'pry'
-require './lib/night_reader.rb'
+require './lib/night_reader'
 
 class NightReaderTest < Minitest::Test
 
@@ -10,19 +10,23 @@ class NightReaderTest < Minitest::Test
     assert_instance_of NightReader, nr
   end
 
-  def test_it_joins_incoming_braille
-    nr = NightReader.new
-    assert_equal "0.....", nr.format_incoming_text("0.\n..\n..")
-  end
-
-  def test_it_creates_array_from_joined_braille_character_string
-    nr = NightReader.new
-    assert_equal ["0.","..", ".."], nr.scan_to_array("0.....")
-  end
+  # def test_it_joins_incoming_braille
+  #   nr = NightReader.new
+  #   assert_equal "0.....", nr.format_incoming_text("0.\n..\n..")
+  # end
 
   def test_braille_corresponds_to_english_letter
     nr = NightReader.new
     assert_equal "z", nr.read_incoming_braille(["0.", ".0", "00"])
+  end
+
+  def test_it_splits_incoming_text_at_line_breaks_into_arrays
+    nr = NightReader.new
+    nr.split("888888\n777777\n666666")
+    assert_equal "888888", nr.line_1
+    assert_equal "777777", nr.line_2
+    assert_equal "666666", nr.line_3
+    #binding.pry
   end
 
 end
